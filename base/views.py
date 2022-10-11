8a13000654f059c6aad36548cf1bf172cad8be79
sapceCabbage <hudafreedman@gmail.com>
2022-10-11 18:23:41 +0300

    update room func

--- base/views.py
+++ base/views.py
@@ -112,34 +112,46 @@ def userProfile(request, pk):
 def createRoom(request):
     
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+
+            
+        )
+        
+        return redirect('home')
         
         
-    context = {'form': form}
+    context = {'form': form, 'topics': topics,}
     return render(request, 'base/room_form.html', context)
 
 
 @login_required(login_url="login")
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
+    topics = Topic.objects.all()
     form = RoomForm(instance=room)
     
     if request.user != room.host:
         return HttpResponse('You are not allowed here.')
     
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
     
-    context = {'form': form}
+    context = {'form': form, 'topics': topics, 'room': room,}
     return render(request, 'base/room_form.html', context)
 
 
@@ -169,3 +181,5 @@ def deleteMessage(request, pk):
         return redirect('home')
     
     return render(request, 'base/delete.html', {'obj': message})
+
+
