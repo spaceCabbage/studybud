c7305b5a69ec1f1d5c8e3167ef897d231cc284a8
yehudafreedman <hudafreedman@gmail.com>
2022-10-26 16:36:29 +0300

    login and register

--- base/views.py
+++ base/views.py
@@ -187,4 +187,11 @@ def deleteMessage(request, pk):
 def updateUser(request):
     user = request.user
     form = UserForm(instance=user)
+    
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            redirect('user-profile', pk=user.id)
+    
     return render(request, 'base/update-user.html', {'form': form})
